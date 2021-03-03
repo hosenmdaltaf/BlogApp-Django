a984b054a31f4920c075857823e2b6ef847defad
hosenmdaltaf <altafhmeazee1999@outlook.com>
2021-03-04 01:34:17 +0800

    make it better

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -45,6 +45,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 ROOT_URLCONF = 'blog.urls'
@@ -120,26 +121,6 @@ USE_L10N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.0/howto/static-files/
-
-# STATIC_URL = '/static/'
-
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR, 'static'),  
-# ]
-# # STATIC_ROOT = os.path.join(BASE_DIR,'static')
-# STATIC_URL = '/static/'
-    
-# if not DEBUG:
-#         STATIC_ROOT = ''
-    
-# STATICFILES_DIRS = [
-#         os.path.join(BASE_DIR, 'static/'),
-#     ]
-
-# MEDIA_URL = '/media/'
-# MEDIA_ROOT=os.path.join(BASE_DIR,'media')
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
@@ -150,6 +131,19 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
+
+# MEDIA_URL = '/media/'
+# MEDIA_ROOT=os.path.join(BASE_DIR,'media')
+
+# # Static files (CSS, JavaScript, Images)
+# # https://docs.djangoproject.com/en/1.9/howto/static-files/
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# STATIC_URL = '/static/'
+
+# # Extra places for collectstatic to find static files.
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, 'static'),
+# )
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT=os.path.join(BASE_DIR,'media')
